bc49594930e1dbbf78658eaaeab0ab996167ec39
tony1819 <37520423+tony1819@users.noreply.github.com>
2018-04-12 23:35:52 +0900

    fadein,fadeout
    로그인페이지 나타내기

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-    $("#login li:first-child").click(function () {
-
+    $("#login").click(function () {
+        $("#loginPage").fadeIn('fast');
+        $("#mask").fadeIn('fast');
+    })
+    $("#sign_up").click(function () {
+        $("#sign_upPage").fadeIn('fast');
+        $("#mask").fadeIn('fast');
+    })
+    $(".exit").click(function(){
+        $("#loginPage").fadeOut('fast');
+        $("#sign_upPage").fadeOut('fast');
+        $("#mask").fadeOut('fast');
     })
 })
